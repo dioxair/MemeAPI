b8c8b173fcc132ae0f4350b426cbee21ff4343ff
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-09 12:50:47 +0000

    Added a logger

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use(function logger(req, res, next) {
+	console.log(new Date().toString())
+	console.log("")
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
 app.get("/", (req, res) => {
 	res.send(">w<")
 });
