fb75bc02e7d70ee7c8ad0b403f3650abfc3250e0
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-09 17:26:46 +0000

    Added 9 more memes to the API

--- memes.js
+++ memes.js
@@ -2,5 +2,14 @@ module.exports = function() {
     this.memeLinks = [
         "https://www.dl.dropboxusercontent.com/s/irwl93qu1xjs1qd/1.mp4?dl=0",
 				"https://www.dl.dropboxusercontent.com/s/acnc3mgjowl2q7l/2.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/bmmosgx0b8l2g7t/3.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/oju3l5njvez7w5k/4.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/mmnyuli5l3hgvi9/5.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/y0t92s1f2u34hwl/7.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/fq3wj105blztg08/8.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/qunxbaapmpylj3s/9.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/l2chatwac0vuy2i/10.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/rcluabjqxe1mp2v/11.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/5xkiudpinzxcpk8/12.mp4?dl=0",
     ]
 }
