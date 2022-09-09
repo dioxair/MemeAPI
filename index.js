0e9660b2a411abaf2611bfdf94cbcce5cc00c8aa
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-09 14:21:09 +0000

    Added additional space in logger

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(function logger(req, res, next) {
 	console.log(new Date().toString())
 	console.log("")
 	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	console.log("")
 	next();
 });
 
