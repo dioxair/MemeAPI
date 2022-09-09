eec97fd5f3e003fc769744f822f50099d9bc2c43
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-09 16:54:08 +0000

    Added 1 more meme

--- memes.js
+++ memes.js
@@ -1,5 +1,6 @@
 module.exports = function() {
     this.memeLinks = [
         "https://www.dl.dropboxusercontent.com/s/irwl93qu1xjs1qd/1.mp4?dl=0",
+				"https://www.dl.dropboxusercontent.com/s/acnc3mgjowl2q7l/2.mp4?dl=0",
     ]
 }
