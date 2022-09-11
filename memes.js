0ced5f913da7fb744c014f4285b5d1f3168c02f8
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-09-11 16:44:43 +0000

    Added 9 more memes

--- memes.js
+++ memes.js
@@ -1,15 +1,24 @@
 module.exports = function() {
-    this.memeLinks = [
-        "https://www.dl.dropboxusercontent.com/s/irwl93qu1xjs1qd/1.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/acnc3mgjowl2q7l/2.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/bmmosgx0b8l2g7t/3.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/oju3l5njvez7w5k/4.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/mmnyuli5l3hgvi9/5.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/y0t92s1f2u34hwl/7.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/fq3wj105blztg08/8.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/qunxbaapmpylj3s/9.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/l2chatwac0vuy2i/10.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/rcluabjqxe1mp2v/11.mp4?dl=0",
-				"https://www.dl.dropboxusercontent.com/s/5xkiudpinzxcpk8/12.mp4?dl=0",
-    ]
+	this.memeLinks = [
+		"https://www.dl.dropboxusercontent.com/s/irwl93qu1xjs1qd/1.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/acnc3mgjowl2q7l/2.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/bmmosgx0b8l2g7t/3.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/oju3l5njvez7w5k/4.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/mmnyuli5l3hgvi9/5.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/y0t92s1f2u34hwl/7.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/fq3wj105blztg08/8.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/qunxbaapmpylj3s/9.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/l2chatwac0vuy2i/10.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/rcluabjqxe1mp2v/11.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/5xkiudpinzxcpk8/12.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/nohga87b0g90kwh/13.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/1tlbt212eb8y2ju/14.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/hnq8rtk81hylsbu/15.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/rtwsxcl62vaxppw/16.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/ih0765fpo4tn17w/17.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/u29z6jmj0xqy32k/18.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/9vqnth70n2g02l2/19.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/odwsmbff7ozgmu6/20.mp4?dl=0",
+		"https://www.dl.dropboxusercontent.com/s/aiu1288udy9i6aa/21.mp4?dl=0",
+	]
 }
